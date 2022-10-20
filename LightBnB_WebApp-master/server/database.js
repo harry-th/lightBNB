27ba620824c120adafbef7a858e10cf6ac8ef193
harry thomas <harryt1905@gmail.com>
2022-10-20 12:36:01 -0400

    reservations list render query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -67,7 +67,15 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT * FROM reservations
+  join properties on property_id = properties.id
+  guest_id = $1 limit $2`,[guest_id, limit])
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
